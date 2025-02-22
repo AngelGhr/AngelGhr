d971e04d922d2f862667b7de2068ef2570d8d855
Angel Garza <34136528+AngelGhr@users.noreply.github.com>
2025-02-22 22:44:18 +0000

    AngelGhr - Install NextJs and basic template and upgrade tailwind to v4

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./mdx-components.tsx",
+		"content/**/*.mdx",
+	],
+
+	theme: {
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						"code::before": {
+							content: '""',
+						},
+						"code::after": {
+							content: '""',
+						},
+					},
+				},
+				quoteless: {
+					css: {
+						"blockquote p:first-of-type::before": { content: "none" },
+						"blockquote p:first-of-type::after": { content: "none" },
+					},
+				},
+			},
+			fontFamily: {
+				sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+				display: ["var(--font-calsans)"],
+			},
+			backgroundImage: {
+				"gradient-radial":
+					"radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [
+		require("@tailwindcss/typography"),
+	],
+};
