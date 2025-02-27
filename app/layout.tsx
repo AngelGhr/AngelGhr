e105715e46637cebe0e930755bb172cf23881835
Angel Garza <34136528+AngelGhr@users.noreply.github.com>
2025-02-27 16:56:27 +0000

    AngelGhr - Add google adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,31 @@
-import "../global.css";
-import { Inter } from "@next/font/google";
-import LocalFont from "@next/font/local";
-import { Metadata } from "next";
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import '../global.css'
+import { Inter } from '@next/font/google'
+import LocalFont from '@next/font/local'
+import { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata: Metadata = {
   title: {
-    default: "AngelGhr",
-    template: "%s | AngelGhr",
+    default: 'AngelGhr',
+    template: '%s | AngelGhr',
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: 'Co-founder of unkey.dev and founder of planetfall.io',
   openGraph: {
-    title: "AngelGhr",
+    title: 'AngelGhr',
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://angelghr.de",
-    siteName: "AngelGhr",
+      'Co-founder of unkey.dev and founder of planetfall.io',
+    url: 'https://angelghr.de',
+    siteName: 'AngelGhr',
     images: [
       {
-        url: "https://angelghr.de/og.png",
+        url: 'https://angelghr.de/og.png',
         width: 1920,
         height: 1080,
       },
     ],
-    locale: "en-US",
-    type: "website",
+    locale: 'en-US',
+    type: 'website',
   },
   robots: {
     index: true,
@@ -33,41 +33,43 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
     },
   },
   twitter: {
-    title: "angelghr",
-    card: "summary_large_image",
+    title: 'angelghr',
+    card: 'summary_large_image',
   },
   icons: {
-    shortcut: "/favicon.png",
+    shortcut: '/favicon.png',
   },
-};
+}
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 const calSans = LocalFont({
-  src: "../public/fonts/CalSans-SemiBold.ttf",
-  variable: "--font-calsans",
-});
+  src: '../public/fonts/CalSans-SemiBold.ttf',
+  variable: '--font-calsans',
+})
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+    <html lang='en' className={[inter.variable, calSans.variable].join(' ')}>
       <head>
-        <meta name="google-adsense-account" content="ca-pub-1988332417674550" />
+        <meta name='google-adsense-account' content='ca-pub-1988332417674550' />
+        <script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1988332417674550'
+     crossOrigin='anonymous'></script>
       </head>
       <body
-        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        className={`bg-black ${process.env.NODE_ENV === 'development' ? 'debug-screens' : undefined
           }`}
       >
         {children}
@@ -75,5 +77,5 @@ export default function RootLayout({
         <SpeedInsights />
       </body>
     </html>
-  );
+  )
 }
