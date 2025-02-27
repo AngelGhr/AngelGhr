d3d4aedd96bbd4c02e8ee1eaa9615e63830be2c8
Angel Garza <34136528+AngelGhr@users.noreply.github.com>
2025-02-27 17:54:13 +0000

    AngelGhr - Setup icons and social media sections

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,56 +1,75 @@
-"use client";
-import { Github, Mail, Twitter } from "lucide-react";
-import Link from "next/link";
-import { Navigation } from "../components/nav";
-import { Card } from "../components/card";
+'use client'
+import { Github, Instagram, Mail, Twitter } from 'lucide-react'
+import Link from 'next/link'
+import { Navigation } from '../components/nav'
+import { Card } from '../components/card'
+import Image from 'next/image'
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/angelghr_",
-		label: "Twitter",
-		handle: "@angelghr_",
+		icon: 'instagram.svg',
+		href: 'https://instagram.com/AngelGhr',
+		label: 'Instagram',
+		handle: '@AngelGhr',
 	},
 	{
-		icon: <Mail size={20} />,
-		href: "mailto:dev@angelghr.de",
-		label: "Email",
-		handle: "dev@angelghr.de",
+		icon: 'tiktok.svg',
+		href: 'https://tiktok.com/@angelghrmx',
+		label: 'TikTok',
+		handle: '@AngelGhrMx',
 	},
 	{
-		icon: <Github size={20} />,
-		href: "https://github.com/angelghr",
-		label: "Github",
-		handle: "angelghr",
+		icon: 'facebook.svg',
+		href: 'https://facebook.com/AngelGhr',
+		label: 'Facebook',
+		handle: '/AngelGhr',
 	},
-];
+	{
+		icon: 'linkedin.svg',
+		href: 'https://www.linkedin.com/in/angelghr',
+		label: 'LinkedIn',
+		handle: '/angelghr',
+	},
+	{
+		icon: 'youtube.svg',
+		href: 'https://youtube.com/@AngelGhrMx',
+		label: 'YouTube',
+		handle: '@AngelGhrMx',
+	},
+	{
+		icon: 'buymeacoffee.svg',
+		href: 'https://www.buymeacoffee.com/AngelGhr',
+		label: 'Buy me a coffee',
+		handle: '@AngelGhr',
+	}
+]
 
 export default function Example() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+		<div className=' bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0'>
 			<Navigation />
-			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
-					{socials.map((s) => (
+			<div className='container flex items-center justify-center min-h-screen px-4 mx-auto'>
+				<div className='grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16'>
+					{socials.map((social) => (
 						<Card>
 							<Link
-								href={s.href}
-								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+								href={social.href}
+								target='_blank'
+								className='p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-16 md:p-16'
 							>
 								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-									aria-hidden="true"
+									className='absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent'
+									aria-hidden='true'
 								/>
-								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-									{s.icon}
-								</span>{" "}
-								<div className="z-10 flex flex-col items-center">
-									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
-										{s.handle}
+								<span className='relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange'>
+									<Image src={`/icons/${social.icon}`} width={48} height={48} alt={social.label} title={social.label} className='invert' />
+								</span>
+								<div className='z-10 flex flex-col items-center'>
+									<span className='lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display'>
+										{social.handle}
 									</span>
-									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-										{s.label}
+									<span className='mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200'>
+										{social.label}
 									</span>
 								</div>
 							</Link>
@@ -59,5 +78,5 @@ export default function Example() {
 				</div>
 			</div>
 		</div>
-	);
+	)
 }
